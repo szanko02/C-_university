1c42532f832c3fc7b03bf974741d77f61d30605f
Stanislav <71770052+szanko02@users.noreply.github.com>
2022-11-24 05:32:54 +0300

    lab5

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+class B
+{
+    private int val;
+
+    public B(int value)
+    {
+        this.val = value;
+    }
+
+    public static bool operator true(B obj)
+    {
+        return obj.val > 0;
+    }
+
+    public static bool operator false(B obj)
+    {
+        return obj.val < 0;
+    }
+
+    public static bool operator &(B obj, int value)
+    {
+        if (obj.val == value)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        B obj = new B(10);
+        Console.WriteLine("More than zero?");
+        if (obj)
+        {
+            Console.WriteLine("True");
+        }
+        else
+        {
+            Console.WriteLine("False");
+        }
+
+        bool result = obj & 10;
+        Console.WriteLine($"Is equal?\n{result}");
+    }
+}
